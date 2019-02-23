24d567117e8468cfa40e046997a674913ac129c5
oneufology <oneufology@gmail.com>
2019-02-23 11:06:07 +0200

    tt cup layout

---
+++ dudenko/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DudenkoConfig(AppConfig):
+    name = 'dudenko'
--- randish/settings.py
+++ randish/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'registration',
     'about',
     'ttcup',
+    'dudenko',
 ]
 
 MIDDLEWARE = [
